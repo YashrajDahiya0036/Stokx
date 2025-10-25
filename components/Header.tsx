d19819549bfeb911125b119aa298095d59c525e5
Yashraj <yashrajdahiya0036@gmail.com>
2025-10-25 18:04:49 +0530

    Added search functionality for stocks

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import Navitems from "./Navitems";
 import UserDropdown from "./UserDropdown";
+import { searchStocks } from "@/lib/actions/finnhub.action";
 
-const Header = ({ user }: { user: User }) => {
+const Header = async ({ user }: { user: User }) => {
+	const initialStocks = await searchStocks()
 	return (
 		<header className="sticky top-0 header">
 			<div className="container header-wrapper">
@@ -17,10 +19,10 @@ const Header = ({ user }: { user: User }) => {
 					/>
 				</Link>
 				<nav className="hidden sm:block">
-					<Navitems />
+					<Navitems initialStocks={initialStocks} />
 				</nav>
 
-				<UserDropdown user={user} />
+				<UserDropdown user={user} initialStocks={initialStocks} />
 			</div>
 		</header>
 	);
